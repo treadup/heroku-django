d3692a8e795aba98a00bfb7267e3bee5f54fe27f
Henrik Treadup <henrik.treadup@gmail.com>
2021-04-24 08:07:33 +0200

    Create StoredFile model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
+import os
 from django.db import models
 
-# Create your models here.
+class StoredFile(models.Model):
+    file = models.FileField(upload_to="files")
+
+    def filename(self):
+        return os.path.basename(self.file.name)
+
+    def path(self):
+        return self.file.path
+
+    def url(self):
+        return self.file.url
+
+    def __str__(self):
+        if self.file:
+            return self.file.name
+        return f"StoredFile ({self.pk})"

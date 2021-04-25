a377c9a355bf77657b65ac4054b7970f1e2780da
Henrik Treadup <henrik.treadup@gmail.com>
2021-04-25 15:48:01 +0200

    Add logout view

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from website import views
 
 urlpatterns = [
+    path("logout/", views.logout_view, name="logout"),
     path("", views.index_view, name="index")
 ]
--- website/views.py
+++ website/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import logout
 
 def index_view(request):
     return render(request, "website/index.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("index")

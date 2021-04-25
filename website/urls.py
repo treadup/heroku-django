909cda2a56e47453c35a602c414ba2d882e8ea0a
Henrik Treadup <henrik.treadup@gmail.com>
2021-04-25 16:20:37 +0200

    Add login view

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from website import views
 
 urlpatterns = [
+    path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("", views.index_view, name="index")
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
 
 def index_view(request):
     return render(request, "website/index.html")
@@ -7,3 +7,15 @@ def index_view(request):
 def logout_view(request):
     logout(request)
     return redirect("index")
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        redirect_url = request.GET.get("next", "/")
+        if user is not None:
+            login(request, user)
+            return redirect(redirect_url)
+
+    return render(request, 'website/login.html')

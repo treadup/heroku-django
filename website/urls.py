7aac3bd57663c0a77d54f1069cf4982185926b40
Henrik Treadup <henrik.treadup@gmail.com>
2021-04-25 18:14:38 +0200

    Add file view
    Add login protected file view. The file view is used to download files.

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,7 @@ from website import views
 urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("files/<str:filename>", views.file_view, name="file"),
     path("files/", views.files_view, name="files"),
     path("", views.index_view, name="index")
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.http import FileResponse, HttpResponseNotFound
 
 from website.models import StoredFile
 
@@ -30,3 +31,12 @@ def files_view(request):
         "files": files,
     }
     return render(request, 'website/files.html', context=context)
+
+@login_required
+def file_view(request, filename):
+    files = list(StoredFile.objects.filter(file=f"files/{filename}"))
+
+    if len(files) == 0:
+        return HttpResponseNotFound(f"<h1>File not found: {filename}</h1>")
+    file = files[0]
+    return FileResponse(open(file.path(), "rb"))

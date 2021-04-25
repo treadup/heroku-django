0b22f6a9d0f0d82a0dbcbfad8a9000b35472a21f
Henrik Treadup <henrik.treadup@gmail.com>
2021-04-25 17:38:05 +0200

    Add files/ page
    Add a login protected files/ page. The plan is to have the files/ page
    display a list of files.

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,6 @@ from website import views
 urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("files/", views.files_view, name="files"),
     path("", views.index_view, name="index")
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def index_view(request):
     return render(request, "website/index.html")
@@ -19,3 +20,7 @@ def login_view(request):
             return redirect(redirect_url)
 
     return render(request, 'website/login.html')
+
+@login_required
+def files_view(request):
+    return render(request, 'website/files.html')
